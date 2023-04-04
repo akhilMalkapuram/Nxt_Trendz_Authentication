d2b3b53c1957d876a8c9242f6f40b89c4d5a0cc4
akhilMalkapuram <malkapuramakhil2196@gmail.com>
2023-04-04 19:51:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
-
-import LoginForm from './components/LoginForm'
-import Home from './components/Home'
-import Products from './components/Products'
-import Cart from './components/Cart'
-import NotFound from './components/NotFound'
-import ProtectedRoute from './components/ProtectedRoute'
-
+import RegistrationForm from './components/RegistrationForm'
 import './App.css'
 
-const App = () => (
-  <BrowserRouter>
-  <Routes>
-    <Route exact path="/login" element={<LoginForm/>} />
-    <ProtectedRoute exact path="/" element={<Home/>} />
-    <ProtectedRoute exact path="/products" element={<Products/>} />
-    <ProtectedRoute exact path="/cart" element={<Cart/>} />
-    <Route element={<NotFound/>} />
-  </Routes>
-  </BrowserRouter>
-)
+const App = () => <RegistrationForm />
 
 export default App
